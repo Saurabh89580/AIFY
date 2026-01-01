fdd31982fadd7f63fa39f8e697bac830501d557a
Saurabh89580 <saurabhdhami56@gmail.com>
2026-01-01 13:00:23 +0530

    Node-templating

--- src/features/executions/components/http-request/executor.ts
+++ src/features/executions/components/http-request/executor.ts
@@ -1,11 +1,19 @@
 import type { NodeExecutor } from "@/features/executions/types"
 import { NonRetriableError } from "inngest";
 import ky , {type Options as KyOptions} from "ky";
+import Handlebars from "handlebars"
+
+Handlebars.registerHelper("json", (context) => {
+    const jsonString= JSON.stringify(context,null,2);
+    const safeString=new Handlebars.SafeString(jsonString);
+
+    return safeString;
+});
 
 type HttpRequestData={
-    variableName?:string;
-    endpoint?:string;
-    method?:"GET" | "POST" | "PUT" | "PATCH"| "DELETE";
+    variableName:string;
+    endpoint:string;
+    method:"GET" | "POST" | "PUT" | "PATCH"| "DELETE";
     body?:string;
 };
 
@@ -24,14 +32,20 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
         throw new NonRetriableError(`HTTP Request node ${nodeId} is missing a variable name`);
     }
 
+     if(!data.method){
+        throw new NonRetriableError(`HTTP Request node ${nodeId} is missing a method`);
+    }
+
     const result=await step.run("http-request",async () => {
-        const endpoints=data.endpoint!;
-        const method=data.method || "GET";
+        const endpoints=Handlebars.compile(data.endpoint)(context);
+        const method=data.method ;
 
         const options:KyOptions={method};
 
         if(["POST","PUT","PATCH"].includes(method) && data.body){
-            options.body=data.body;
+            const resolved =Handlebars.compile(data.body || "{}")(context);
+            JSON.parse(resolved);
+            options.body=resolved;
             options.headers={
                 "Content-Type": "application/json"
             }
@@ -49,15 +63,9 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
                 data:responseData, 
             },
         }
-        if(data.variableName){
-        return {
-            ...context,
-            [data.variableName]: responsePayLoad,
-            }
-        }
         return {
         ...context,
-        ...responsePayLoad, 
+        [data.variableName]:responsePayLoad, 
         }
     });
 
