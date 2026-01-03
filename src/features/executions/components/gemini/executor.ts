4d9c50fae675382078722c7f332a4b69df19ca41
Saurabh89580 <saurabhdhami56@gmail.com>
2026-01-03 21:24:21 +0530

    AI-model

---
+++ src/features/executions/components/gemini/executor.ts
@@ -0,0 +1,108 @@
+import type { NodeExecutor } from "@/features/executions/types"
+import { gemini, NonRetriableError } from "inngest";
+import {createGoogleGenerativeAI} from "@ai-sdk/google";
+import Handlebars from "handlebars"
+import { geminiChannel } from "@/inngest/channels/gemini";
+import { generateText } from "ai";
+
+Handlebars.registerHelper("json", (context) => {
+    const jsonString= JSON.stringify(context,null,2);
+    const safeString=new Handlebars.SafeString(jsonString);
+
+    return safeString;
+});
+
+type GeminiData={
+    variableName?:string;
+    systemPrompt?:string;
+    userPrompt?:string;
+};
+
+export const geminiExecutor: NodeExecutor<GeminiData> = async ({ 
+    data,
+    nodeId, 
+    context, 
+    step,
+    publish,
+}) => {
+    await publish(
+        geminiChannel().status({
+            nodeId,
+            status: "loading",
+        }),
+    );
+
+    if(!data.variableName){
+        await publish(
+            geminiChannel().status({
+                nodeId,
+                status: "error",
+            }),
+        );
+        throw new NonRetriableError("Gemini node: Variable name is required");
+    }
+    if (!data.userPrompt) {
+        await publish(
+            geminiChannel().status({
+                nodeId,
+                status: "error",
+            }),
+        );
+        throw new NonRetriableError("Gemini node: User prompt is required");
+    }
+
+    const systemPrompt=data.systemPrompt
+        ? Handlebars.compile(data.systemPrompt)(context)
+        : "Your are a helpful assistant.";
+    const userPrompt=Handlebars.compile(data.userPrompt)(context);
+
+    const credentialValue = process.env.GOOGLE_GENERATIVE_AI_API_KEY!;
+
+    const google = createGoogleGenerativeAI({
+        apiKey: credentialValue,
+    });
+
+    try{
+        const{ steps } = await step.ai.wrap(
+            "gemini-generate-text",
+            generateText,
+            {
+                model:google("gemini-flash-latest"), 
+                system:systemPrompt,
+                prompt:userPrompt,
+                experimental_telemetry:{
+                    isEnabled:true,
+                    recordInputs:true,
+                    recordOutputs:true,
+                }
+            },
+        )
+        const text=
+         steps[0].content[0].type === "text"
+         ?
+         steps[0].content[0].text
+         :"";
+
+         await publish(
+            geminiChannel().status({
+                nodeId,
+                status: "success",
+            }),
+         );
+
+         return {
+            ...context,
+            [data.variableName ]: {
+                text,
+            },
+         }
+    }catch(error){
+        await publish(
+            geminiChannel().status({
+                nodeId,
+                status: "error",
+            }),
+         );
+         throw error;
+    }
+};
